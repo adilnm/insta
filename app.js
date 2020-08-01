58db47bf80a67981441a3dd8af44e204d0396131
Adil Naimi <adilnm23@hotmail.com>
2020-07-31 21:56:41 -0500

    Indentation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const { MONGOURI } = require("./config/keys");
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,12 +21,12 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('client/build'))
-  const path = require('path')
-  app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
